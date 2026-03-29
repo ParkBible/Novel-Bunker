625cfe506fe6671b135871c20c7b814bb661644d
ParkBible <p8yft8903@gmail.com>
2026-03-29 17:53:47 +0900

    feat: 구글 드라이브 동기화(수동 업로드/다운로드) 기능 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { DevInspector } from "./(shared)/components/DevInspector";
+import { GoogleAuthScript } from "./(shared)/components/GoogleAuthScript";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
+                <GoogleAuthScript />
                 <DevInspector />
                 {children}
             </body>
